20d8ae8ce0aeeafaaefa2243f58bf35f71417b06
D.Leap <dilipkumar2000.r@gmail.com>
2025-12-25 20:34:31 +0530

    fix cors

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   server:[{
-    '/api':'http://localhost:8080/'
+    '/api':'freddy-ten.vercel.app'
   }],
   plugins: [
     react(),
